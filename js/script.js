5e7f4206d05a2a85d188f15567fc499a142aa6d1
marie_l <mariemlaqua@gmail.com>
2022-05-28 11:08:29 -0700

    Add const and async funct to fetch repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //div where profile info will appear
 const overview = document.querySelector(".overview");
 const username = "Marie-m-l";
+//ul where the repos from gitHub display
+const repoLs = document.querySelector("repo-list");
 
 //fetch API json data
 const getUserInfo = async function () {
@@ -25,6 +27,14 @@ const displayFetchedData = function (data) {
         <p><strong>Name:</strong> ${data.name}<p>
         <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
-        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>`;
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+    `;
     overview.append(div);
+};
+
+// async to fetch repos
+const getRepoLs = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
 };
